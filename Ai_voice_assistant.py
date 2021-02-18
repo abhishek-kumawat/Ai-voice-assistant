e8613cc2056633a03e5949b77094edeb5d824abf
Abhishek kumawat <60384784+abhishek-kumawat@users.noreply.github.com>
2021-02-18 14:18:57 +0530

    Update Ai_voice_assistant.py

--- Ai_voice_assistant.py
+++ Ai_voice_assistant.py
@@ -1,7 +1,7 @@
-import pyttsx3 #pip install pyttsx3 
+import pyttsx3  
 import datetime 
 import speech_recognition as sr 
-import wikipedia #pip install wikipedia
+import wikipedia 
 import smtplib
 import webbrowser as wb
 import os
